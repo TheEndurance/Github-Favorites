86b3fdb95bab7a32948ff06f2d7ded4033a0b961
TheEndurance <rawa.jalal@gmail.com>
2018-09-15 22:09:03 -0400

    first commit

---
+++ src/FavoritesDashboard.js
@@ -0,0 +1,10 @@
+import {Component} from 'react';
+
+export default class FavoritesDashboard extends Component {
+    render(){
+        return(
+            <div></div>
+        )
+    }
+}
+
---
+++ src/RepoList.js
@@ -0,0 +1,9 @@
+import { Component } from 'react';
+
+export default class RepoList extends Component {
+    render(){
+        return(
+            <div></div>
+        )
+    }
+}
